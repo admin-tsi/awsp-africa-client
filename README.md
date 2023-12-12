a6a18e1f72bdc69a6c2ca58aef95f78f411e7dd6
Jordan Vitou <71874111+CodeBreaker02@users.noreply.github.com>
2023-12-12 10:30:10 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# awsp-africa-client
---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+
+const sairaFont = localFont({
+  variable: '--saira-font',
+  display: 'swap',
+  src: [
+    {
+      path: '../fonts/Saira-VariableFont_wdth,wght.ttf',
+      weight: 'variable',
+    },
+    {
+      path: '../fonts/Saira-Italic-VariableFont_wdth,wght.ttf',
+      weight: 'variable',
+    },
+  ],
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <main className={`${sairaFont.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  );
+}
