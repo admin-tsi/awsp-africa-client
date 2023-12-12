6c65096e3e5eb62f276676b2b3e64328d03d87b8
Bitsmith01 <esperat.dimon@epitech.eu>
2023-12-12 17:24:56 +0100

    [Set up]: Setting Footer componnent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import CustomButton from '@/components/CustomButton';
+import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 
 export default function Index() {
   return (
     <div>
       <Navbar />
+      <Footer />
     </div>
   );
 }
