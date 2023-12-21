c9f8a99a49db92384d9bee36a4424921e7142269
codebreaker02 <jordan.vitou@epitech.eu>
2023-12-21 16:49:36 +0100

    feature: homepage and responsivity

---
+++ src/components/home/Testimonial.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Image from 'next/image';
+
+interface TestimonialProps {
+  name: string;
+  title: string;
+  company: string;
+  testimonial: string;
+  imageSrc: string;
+}
+
+const Testimonial: React.FC<TestimonialProps> = ({
+  name,
+  title,
+  company,
+  testimonial,
+  imageSrc,
+}) => {
+  return (
+    <div className="bg-white p-6 shadow-lg rounded-lg text-center relative">
+      <Image
+        src={imageSrc}
+        alt={name}
+        width={100}
+        height={100}
+        className="rounded-full  absolute -translate-y-1/2 -translate-x-1/2 top-0 left-1/2"
+      />
+      <div className="mt-8">
+        <p className="text-lg font-light text-gray-600">{testimonial}</p>
+        <div className="mt-4">
+          <p className="text-xl font-semibold">{name}</p>
+          <p className="text-lg font-semibold text-black">
+            {title}, {company}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
