2b4590711b5a5608236f2c3b053e1532afbf46bd
Bitsmith01 <esperat.dimon@epitech.eu>
2024-01-02 09:08:02 +0100

    RAS

--- src/Api/kkiapay.service.ts
+++ src/Api/kkiapay.service.ts
@@ -7,7 +7,7 @@ export const openingKKkiaWidget = (email : string) => {
       amount: 1,
       api_key: process.env.NEXT_PUBLIC_KKIPAY_KEY,
       email: email,
-      callback: `http://127.0.0.1:5010/api/v1/payments/KkiaPay/callBack?${url}`,
+      callback: `https://7140-2c0f-2a80-fc-8610-f04d-c11a-3505-973.ngrok-free.app/api/v1/payments/kkiapay/callback?${url}`,
       theme:"#000000"
    })
 }
