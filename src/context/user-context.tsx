43f5dbababcbb3bb7ce2f5526b5bccaa697a2cbb
codebreaker02 <jordan.vitou@epitech.eu>
2024-02-05 09:45:10 +0100

    update: add token

--- src/context/user-context.tsx
+++ src/context/user-context.tsx
@@ -102,7 +102,7 @@ export const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
       }
 
       const data = await response.json();
-      localStorage.setItem('token', data.token.token);
+      localStorage.setItem('credentials', data.token.token);
       console.log(data.user.isverified);
 
       setCookie(
